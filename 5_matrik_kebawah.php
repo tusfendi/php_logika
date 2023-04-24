c74b193edc0569c515d2af9be528c3ad1ffb0811
tusfendi <tusfendi@gmail.com>
2023-04-24 21:04:13 +0700

    update soal 5

--- 5_matrik_kebawah.php
+++ 5_matrik_kebawah.php
@@ -1,7 +1,24 @@
 <?php
 
 $n = 12;
+$array = [];
+$index = 1;
 for ($i=1; $i <= $n; $i++) { 
+    if ($index <= 4) {
+        $array[$index][] = $i;
+        $index++;
+    } else {
+        $index = 1; 
+        $array[$index][] = $i;
+        $index++;
+    }
+}
+
+foreach ($array as $values) {
+    foreach ($values as $value) {
+        echo $value . ' ';
+    }
+    echo "\n";
 }
 
 ?>
